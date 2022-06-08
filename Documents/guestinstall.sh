64e01b3127dfe01f11d2818c1122fc2706335750
purplebeartoo <bcrypt@runbox.com>
2022-06-08 14:57:09 -0500

    replace eog with viewnior

--- Documents/guestinstall.sh
+++ Documents/guestinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #guestinstall
 
-pacman -S apparmor chromium dconf-editor eog evince firejail gdm git gnome-control-center gnome-keyring gnome-shell gnome-system-monitor gnome-terminal gnome-themes-extra gnome-tweaks imagemagick keepassxc linux-headers lsof nautilus neovim noto-fonts noto-fonts-emoji papirus-icon-theme python-pywal reflector ttf-dejavu ttf-liberation ttf-opensans ufw wl-clipboard xclip xdg-user-dirs
+pacman -S apparmor chromium dconf-editor evince firejail gdm git gnome-control-center gnome-keyring gnome-shell gnome-system-monitor gnome-terminal gnome-themes-extra gnome-tweaks imagemagick keepassxc linux-headers lsof nautilus neovim noto-fonts noto-fonts-emoji papirus-icon-theme python-pywal reflector ttf-dejavu ttf-liberation ttf-opensans ufw viewnior wl-clipboard xclip xdg-user-dirs
 
 echo "Guest packages installed ;)"
