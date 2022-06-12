4b6fc44d7c822f398c68b625c3a3ab3545669493
purplebeartoo <bcrypt@runbox.com>
2022-06-12 08:36:52 -0500

    reverse edits

--- Documents/hostinstall.sh
+++ Documents/hostinstall.sh
@@ -1,1 +1,6 @@
-apparmor baobab bat clinfo cronie dconf-editor evince exa ffmpeg file-roller firejail flatpak foot fzf gdm gimp git gnome-calculator gnome-characters gnome-control-center gnome-keyring gnome-multi-writer gnome-shell gnome-shell-extensions gnome-system-monitor gnome-themes-extra gnome-tweaks gparted handlr htop imagemagick imv keepassxc libmad libva-mesa-driver libva-utils linux-headers lsof man-db meld mesa mpd mpv nautilus ncmpcpp neovim noto-fonts noto-fonts-emoji papirus-icon-theme python-pywal radeontop ranger reflector resolvconf ripgrep tracker ttf-dejavu ttf-liberation ttf-opensans ufw wl-clipboard xclip xdg-desktop-portal-gnome xdg-user-dirs zip zsh zsh-autosuggestions zsh-syntax-highlighting
+#!/bin/sh    
+#hostinstall    
+    
+sudo pacman -S apparmor baobab bat clinfo cronie dconf-editor evince exa ffmpeg file-roller firejail flatpak foot fzf gdm gimp git gnome-calculator gnome-characters gnome-control-center gnome-keyring gnome-multi-writer gnome-shell gnome-shell-extensions gnome-system-monitor gnome-themes-extra gnome-tweaks gparted handlr htop imagemagick imv keepassxc libmad libva-mesa-driver libva-utils linux-headers lsof man-db meld mesa mpd mpv nautilus ncmpcpp neovim noto-fonts noto-fonts-emoji papirus-icon-theme python-pywal radeontop ranger reflector resolvconf ripgrep tracker ttf-dejavu ttf-liberation ttf-opensans ufw wl-clipboard xclip xdg-desktop-portal-gnome xdg-user-dirs zip zsh zsh-autosuggestions zsh-syntax-highlighting
+
+echo "Host packages installed ;)"
