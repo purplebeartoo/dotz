92c872e246ec311c7c6969a9948ab839254dcf2e
purplebeartoo <bcrypt@runbox.com>
2022-06-23 20:34:02 -0500

    add xsecurelock xss-lock

--- Documents/hostinstall.sh
+++ Documents/hostinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh    
 #hostinstall    
     
-sudo pacman -S alacritty apparmor ckb-next cmus-git cronie exa firefox-nightly firejail fzf gimp git gvfs handlr hardened_malloc htop imagemagick i3lock imv keepassxc linux-headers man-db meld mpv mupdf neovim nerd-fonts-fira-code nitrogen noto-fonts noto-fonts-emoji numlockx pfetch-git python-pywal radeontop ranger reflector ripgrep sxhkd timeshift ttf-dejavu ttf-liberation ttf-ms-fonts ttf-opensans udisks2 ufw unzip xautolock xclip xdg-user-dirs zip zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S alacritty apparmor ckb-next cmus-git cronie exa firefox-nightly firejail fzf gimp git gvfs handlr hardened_malloc htop imagemagick imv keepassxc linux-headers man-db meld mpv mupdf neovim nerd-fonts-fira-code nitrogen noto-fonts noto-fonts-emoji numlockx pfetch-git python-pywal radeontop ranger reflector ripgrep sxhkd timeshift ttf-dejavu ttf-liberation ttf-ms-fonts ttf-opensans udisks2 ufw unzip xclip xdg-user-dirs xss-lock xsecurelock zip zsh zsh-autosuggestions zsh-syntax-highlighting
 
 echo "Host packages installed ;)"
