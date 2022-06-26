28a9e835d71a5afea2e28c48369c0a79db573871
purplebeartoo <bcrypt@runbox.com>
2022-06-26 11:53:50 -0500

    cleanup

--- Documents/hostinstall.sh
+++ Documents/hostinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh    
 #hostinstall    
     
-sudo pacman -S alacritty apparmor ckb-next cmus-git cronie exa firefox-nightly firejail fzf gimp git gvfs handlr hardened_malloc htop imagemagick imv keepassxc linux-headers lxappearance man-db meld mpv mupdf neovim nerd-fonts-fira-code nitrogen noto-fonts noto-fonts-emoji numlockx pfetch-git python-pywal radeontop ranger reflector ripgrep sxhkd timeshift ttf-dejavu ttf-liberation ttf-ms-fonts ttf-opensans udisks2 ufw unzip xclip xdg-user-dirs xss-lock xsecurelock zip zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S alacritty apparmor cronie exa firefox-nightly firejail fzf gcompris-qt gimp git gvfs handlr htop imagemagick imv keepassxc linux-headers lxappearance man-db meld mpv mupdf neovim nitrogen noto-fonts noto-fonts-emoji numlockx pcmanfm-gtk3 python-pywal radeontop ranger reflector ripgrep scrot sxhkd ttf-dejavu ttf-liberation ttf-opensans udisks2 ufw unzip xclip xdg-user-dirs xss-lock xsecurelock zip zsh zsh-autosuggestions zsh-syntax-highlighting
 
 echo "Host packages installed ;)"
