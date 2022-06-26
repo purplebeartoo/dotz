f49ffdf9f67d7136d3dd14d2e543b93c077d80d2
purplebeartoo <bcrypt@runbox.com>
2022-06-26 17:40:20 -0500

    add galculator

--- Documents/hostinstall.sh
+++ Documents/hostinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh    
 #hostinstall    
     
-sudo pacman -S alacritty apparmor cronie exa firefox-nightly firejail fzf gcompris-qt gimp git gvfs handlr htop imagemagick imv keepassxc linux-headers lxappearance man-db meld mpv mupdf neovim nitrogen noto-fonts noto-fonts-emoji numlockx pcmanfm-gtk3 python-pywal radeontop ranger reflector ripgrep scrot sxhkd ttf-dejavu ttf-liberation ttf-opensans udisks2 ufw unzip xclip xdg-user-dirs xss-lock xsecurelock zip zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S alacritty apparmor cronie exa firefox-nightly firejail fzf galculator gcompris-qt gimp git gvfs handlr htop imagemagick imv keepassxc linux-headers lxappearance man-db meld mpv mupdf neovim nitrogen noto-fonts noto-fonts-emoji numlockx pcmanfm-gtk3 python-pywal radeontop ranger reflector ripgrep scrot sxhkd ttf-dejavu ttf-liberation ttf-opensans udisks2 ufw unzip xclip xdg-user-dirs xss-lock xsecurelock zip zsh zsh-autosuggestions zsh-syntax-highlighting
 
 echo "Host packages installed ;)"
