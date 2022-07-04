65e71b8e9064b16050d3ec56f1e5a771118e0d89
purplebeartoo <bcrypt@runbox.com>
2022-07-04 14:37:16 -0500

    add logrotate

--- Documents/hostinstall.sh
+++ Documents/hostinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh    
 #hostinstall
 
-sudo pacman -S alacritty apparmor chromium cronie exa firejail fzf galculator gcompris-qt gimp git gparted gvfs handlr htop imagemagick imv keepassxc linux-headers lxappearance lxsession-gtk3 man-db meld mpv mupdf neovim nitrogen noto-fonts noto-fonts-emoji numlockx pcmanfm-gtk3 polkit radeontop ranger reflector ripgrep scrot sxhkd ttf-dejavu ttf-liberation ttf-opensans udisks2 ufw unzip xclip xdg-user-dirs xss-lock xsecurelock zip zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S alacritty apparmor chromium cronie exa firejail fzf galculator gcompris-qt gimp git gparted gvfs handlr htop imagemagick imv keepassxc linux-headers logrotate lxappearance lxsession-gtk3 man-db meld mpv mupdf neovim nitrogen noto-fonts noto-fonts-emoji numlockx pcmanfm-gtk3 polkit radeontop ranger reflector ripgrep scrot sxhkd ttf-dejavu ttf-liberation ttf-opensans udisks2 ufw unzip xclip xdg-user-dirs xss-lock xsecurelock zip zsh zsh-autosuggestions zsh-syntax-highlighting
 
 echo "Host packages installed ;)"
