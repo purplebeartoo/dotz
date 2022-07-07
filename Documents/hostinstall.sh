fa06c0f00677822ad55cc81f912894cf1f304afb
purplebeartoo <bcrypt@runbox.com>
2022-07-06 20:17:16 -0500

    add papirus icon theme

--- Documents/hostinstall.sh
+++ Documents/hostinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/sh    
 #hostinstall
 
-sudo pacman -S alacritty apparmor chromium cronie exa firejail fzf galculator gcompris-qt gimp git gparted gvfs handlr htop imagemagick imv keepassxc linux-headers logrotate lxappearance lxsession-gtk3 man-db meld mpv mupdf neovim noto-fonts noto-fonts-emoji numlockx pcmanfm-gtk3 radeontop ranger reflector ripgrep scrot ttf-dejavu ttf-liberation ttf-opensans udisks2 ufw unzip xclip xdg-user-dirs xautolock xsecurelock zip zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S alacritty apparmor chromium cronie exa firejail fzf galculator gcompris-qt gimp git gparted gvfs handlr htop imagemagick imv keepassxc linux-headers logrotate lxappearance lxsession-gtk3 man-db meld mpv mupdf neovim noto-fonts noto-fonts-emoji numlockx papirus-icon-theme pcmanfm-gtk3 radeontop ranger reflector ripgrep scrot ttf-dejavu ttf-liberation ttf-opensans udisks2 ufw unzip xclip xdg-user-dirs xautolock xsecurelock zip zsh zsh-autosuggestions zsh-syntax-highlighting
 
 echo "Host packages installed ;)"
