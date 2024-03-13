95772d1798ebe160080db8dc97f525ea04d9728d
purplebeartoo <bcrypt@proton.me>
2024-03-13 17:19:42 -0500

    rm arch-audit

--- Documents/hostinstall.sh
+++ Documents/hostinstall.sh
@@ -2,7 +2,7 @@
 #hostinstall
 
 #core
-sudo pacman -S alacritty alsa-utils apparmor arch-audit base-devel bat btop cmus compsize cronie dconf-editor duf dunst eza fd ffmpeg firejail flatpak fuzzel fzf galculator gawk git gnome-disk-utility gparted grim gvfs handlr hyprland hyprlang hyprpaper imv inotify-tools keepassxc kvantum kvantum-qt5 libva-mesa-driver linux-headers logrotate man-db meld mesa ncurses nemo neovim nm-connection-editor noto-fonts noto-fonts-emoji pam-u2f pavucontrol perl-image-exiftool playerctl polkit-gnome qt5-wayland qt6-wayland radeontop ranger reflector ripgrep rsync slurp starship swayidle timeshift ttf-dejavu ttf-firacode-nerd ttf-liberation ttf-opensans tumbler ufw unzip vulkan-radeon waybar wl-clipboard xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-user-dirs xf86-video-amdgpu zip zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S alacritty alsa-utils apparmor base-devel bat btop cmus compsize cronie dconf-editor duf dunst eza fd ffmpeg firejail flatpak fuzzel fzf galculator gawk git gnome-disk-utility gparted grim gvfs handlr hyprland hyprlang hyprpaper imv inotify-tools keepassxc kvantum kvantum-qt5 libva-mesa-driver linux-headers logrotate man-db meld mesa ncurses nemo neovim nm-connection-editor noto-fonts noto-fonts-emoji pam-u2f pavucontrol perl-image-exiftool playerctl polkit-gnome qt5-wayland qt6-wayland radeontop ranger reflector ripgrep rsync slurp starship swayidle timeshift ttf-dejavu ttf-firacode-nerd ttf-liberation ttf-opensans tumbler ufw unzip vulkan-radeon waybar wl-clipboard xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-user-dirs xf86-video-amdgpu zip zsh zsh-autosuggestions zsh-syntax-highlighting
 echo "Pacman packages installed."
 
 #flatpaks
