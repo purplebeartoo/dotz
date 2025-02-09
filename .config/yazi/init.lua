46a1e4171ecf186edcb2f0c20ec88a11e79ece7d
purplebeartoo <bcrypt@proton.me>
2025-02-08 21:54:14 -0600

    filename truncation

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -12,4 +12,8 @@ function Linemode:size_and_mtime()
   return string.format("%s %s", size and ya.readable_size(size) or "-", time)
 end
 
-require("yaziline"):setup()
+require("yaziline"):setup({
+  filename_max_length = 48, -- truncate when filename > 24
+  filename_truncate_length = 12, -- leave 6 chars on both sides
+  filename_truncate_separator = "..." -- the separator of the truncated filename
+})
