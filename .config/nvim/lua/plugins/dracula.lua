8c6c4ce5fea300ea6c378044f9b95f8b9a1e0602
purplebeartoo <bcrypt@proton.me>
2025-02-14 23:23:15 -0600

    +ghostty, -kitty

--- .config/nvim/lua/plugins/dracula.lua
+++ .config/nvim/lua/plugins/dracula.lua
@@ -1,78 +1,60 @@
 return {
-  {
-    'maxmx03/dracula.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function ()
-      ---@type dracula
-      local dracula = require "dracula"
-
-      dracula.setup({
-      styles = {
-        Type = {},
-        Function = {},
-        Parameter = {},
-        Property = {},
-        Comment = {},
-        String = {},
-        Keyword = {},
-        Identifier = {},
-        Constant = {},
+  "Mofiqul/dracula.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local dracula = require("dracula")
+    dracula.setup({
+      -- customize dracula color palette
+      colors = {
+        bg = "#282A36",
+        fg = "#F8F8F2",
+        selection = "#44475A",
+        comment = "#6272A4",
+        red = "#FF5555",
+        orange = "#FFB86C",
+        yellow = "#F1FA8C",
+        green = "#50fa7b",
+        purple = "#BD93F9",
+        cyan = "#8BE9FD",
+        pink = "#FF79C6",
+        bright_red = "#FF6E6E",
+        bright_green = "#69FF94",
+        bright_yellow = "#FFFFA5",
+        bright_blue = "#D6ACFF",
+        bright_magenta = "#FF92DF",
+        bright_cyan = "#A4FFFF",
+        bright_white = "#FFFFFF",
+        menu = "#21222C",
+        visual = "#3E4452",
+        gutter_fg = "#4B5263",
+        nontext = "#3B4048",
+        white = "#ABB2BF",
+        black = "#191A21",
       },
-      transparent = false,
-      on_colors = function (colors, color)
-        ---@type dracula.palette
-        return {
-          -- override or create new colors
-          mycolor = "#ffffff",
-          -- mycolor = 0xffffff,
-        }
-      end,
-      on_highlights = function (colors, color)
-        ---@type dracula.highlights
-        return {
-          ---@type vim.api.keyset.highlight
-          Normal = { fg = colors.mycolor }
-        }
-      end,
-      plugins = {
-        ["nvim-treesitter"] = true,
-        ["rainbow-delimiters"] = true,
-        ["nvim-lspconfig"] = true,
-        ["nvim-navic"] = true,
-        ["nvim-cmp"] = true,
-        ["indent-blankline.nvim"] = true,
-        ["neo-tree.nvim"] = true,
-        ["nvim-tree.lua"] = true,
-        ["which-key.nvim"] = true,
-        ["dashboard-nvim"] = true,
-        ["gitsigns.nvim"] = true,
-        ["neogit"] = true,
-        ["todo-comments.nvim"] = true,
-        ["lazy.nvim"] = true,
-        ["telescope.nvim"] = true,
-        ["noice.nvim"] = true,
-        ["hop.nvim"] = true,
-        ["mini.statusline"] = true,
-        ["mini.tabline"] = true,
-        ["mini.starter"] = true,
-        ["mini.cursorword"] = true,
-        ['bufferline.nvim'] = true,
-      }
-      })
-      vim.cmd.colorscheme 'dracula'
-      vim.cmd.colorscheme 'dracula-soft'
-    end
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    opts = {
-      options = {
-        theme = vim.g.colors_name,
-        refresh = {
-          statusline = 1000,
-        },
-      },
-    },
-  }
+      -- show the '~' characters after the end of buffers
+      show_end_of_buffer = true, -- default false
+      -- use transparent background
+      transparent_bg = true, -- default false
+      -- set custom lualine background color
+      lualine_bg_color = "#44475a", -- default nil
+      -- set italic comment
+      italic_comment = true, -- default false
+      -- overrides the default highlights with table see `:h synIDattr`
+      overrides = {},
+      -- You can use overrides as table like this
+      -- overrides = {
+      --  NonText = { fg = "white" }, -- set NonText fg to white
+      --  NvimTreeIndentMarker = { link = "NonText" }, -- link to NonText highlight
+      --  Nothing = {} -- clear highlight of Nothing
+      -- },
+      -- Or you can also use it like a function to get color from theme
+      -- overrides = function (colors)
+      --  return {
+      --   NonText = { fg = colors.white }, -- set NonText fg to white of theme
+      --  }
+      -- end,
+    })
+    vim.cmd.colorscheme("dracula")
+  end,
 }
