baadf057aab5045c062ac4c165d95fd8c9d6599d
purplebeartoo <bcrypt@proton.me>
2025-02-23 12:08:22 -0600

    +bubblejail, -firejail

--- Documents/hostinstall.sh
+++ Documents/hostinstall.sh
@@ -2,7 +2,7 @@
 # Host install
 
 # Pacman
-sudo pacman -S age alsa-utils apparmor base-devel bat brightnessctl btop cmus cronie dconf-editor dunst eza fastfetch fd ffmpeg firejail flatpak fuzzel fzf galculator gcr ghostty git grim gvfs handlr hypridle hyprland hyprlang hyprlock hyprpaper imagemagick inotify-tools keepassxc kvantum kvantum-qt5 libva-mesa-driver libzip linux-headers logrotate luarocks man-db meld mesa nemo neovim nm-connection-editor noto-fonts noto-fonts-emoji pam-u2f pavucontrol pciutils perl-image-exiftool playerctl polkit-gnome python-pillow qt5-wayland qt6-wayland reflector ripgrep rocm-smi-lib rsync slurp starship tela-circle-icon-theme-dracula timeshift ttf-cascadia-code-nerd ttf-dejavu ttf-font-awesome ttf-nerd-fonts-symbols-mono tumbler ufw unzip usbguard usbutils vimiv vulkan-radeon waybar wl-clipboard xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-user-dirs xf86-video-amdgpu yazi zip zsh zsh-autosuggestions zsh-syntax-highlighting
+sudo pacman -S age alsa-utils base-devel bat brightnessctl btop cmus cronie dconf-editor dunst eza fastfetch fd ffmpeg flatpak fuzzel fzf galculator gcr ghostty git grim gvfs handlr hypridle hyprland hyprlang hyprlock hyprpaper imagemagick inotify-tools keepassxc kvantum kvantum-qt5 libva-mesa-driver libzip linux-headers logrotate luarocks man-db meld mesa nemo neovim nm-connection-editor noto-fonts noto-fonts-emoji pam-u2f pavucontrol pciutils perl-image-exiftool playerctl polkit-gnome python-pillow qt5-wayland qt6-wayland reflector ripgrep rocm-smi-lib rsync slirp4netns slurp starship tela-circle-icon-theme-dracula timeshift ttf-cascadia-code-nerd ttf-dejavu ttf-font-awesome ttf-nerd-fonts-symbols-mono tumbler ufw unzip usbguard usbutils vimiv vulkan-radeon waybar wl-clipboard xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-user-dirs xf86-video-amdgpu yazi zip zsh zsh-autosuggestions zsh-syntax-highlighting
 echo "Pacman packages installed."
 
 # Flatpaks
@@ -21,7 +21,7 @@ flatpak install -y --user --noninteractive flathub-beta org.gimp.GIMP
 echo "Flatpak installations complete."
 
 # AUR
-# paru -S brave-bin dracula-gtk-theme otf-apple-fonts rose-pine-cursor rose-pine-hyprcursor waybar-module-pacman-updates-git
+# paru -S brave-bin bubblejail dracula-gtk-theme otf-apple-fonts rose-pine-cursor rose-pine-hyprcursor waybar-module-pacman-updates-git
 
 # Hyprland from source
 # paru -S ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang-git hyprcursor-git hyprwayland-scanner-git xcb-util-errors hyprutils-git hyprgraphics-git aquamarine-git hyprland-qtutils-git re2 glaze 
