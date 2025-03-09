c57498e2c841328016181e16ca84f69a7baa9f4a
purplebeartoo <bcrypt@proton.me>
2025-03-09 10:48:04 -0500

    lazy.nvim checker

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,7 +24,6 @@ require("lazy").setup({
     -- import your plugins
     { import = "plugins" },
   },
-  -- Configure any other settings here. See the documentation for more details.
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = true, notify = false },
 })
