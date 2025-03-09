54991bc98731cf644281dc6137015283a089f94f
purplebeartoo <bcrypt@proton.me>
2025-03-09 10:49:23 -0500

    style has_updates

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,6 +7,9 @@ return {
       lualine_c = {
         {"filename", file_status = true, path = 2,},
       },
+      lualine_x = {
+        { require("lazy.status").updates, cond = require("lazy.status").has_updates, color = { fg = "#ffb86c" }, },
+      },
     }
   end,
 }
