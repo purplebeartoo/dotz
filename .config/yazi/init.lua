f70789db34bc5ab87e0ad47071283d4adc3ba6c9
purplebeartoo <bcrypt@proton.me>
2025-07-12 14:35:20 -0500

    disable Yaziline

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -13,9 +13,4 @@ function Linemode:size_and_mtime()
   return string.format("%s %s", size and ya.readable_size(size) or "-", time)
 end
 
--- Increase filename max length
-require("yaziline"):setup({
-  filename_max_length = 48, -- truncate when filename > 48
-  filename_truncate_length = 12, -- leave 12 chars on both sides
-  filename_truncate_separator = "..." -- the separator of the truncated filename
-})
+require("no-status"):setup()
