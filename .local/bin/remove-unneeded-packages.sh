ca3e7dadb23af297d1d9bb3da3170de903b43bb5
purplebeartoo <bcrypt@proton.me>
2025-07-22 20:32:11 -0500

    mod spacing

--- .local/bin/remove-unneeded-packages.sh
+++ .local/bin/remove-unneeded-packages.sh
@@ -4,15 +4,15 @@
 # Remove orphaned packages
 pacman -Qtdq | sudo xargs -r pacman -Rns 2>/dev/null
 if [ $? -ne 0 ]; then
-    echo "Error occurred while removing orphaned packages. Exiting."
-    exit 1
+  echo "Error occurred while removing orphaned packages. Exiting."
+  exit 1
 fi
 
 # Remove packages not needed by any user
 pacman -Qqd | sudo xargs -r pacman -Rsu 2>/dev/null
 if [ $? -ne 0 ]; then
-    echo "Error occurred while removing unused packages. Exiting."
-    exit 1
+  echo "Error occurred while removing unused packages. Exiting."
+  exit 1
 fi
 
 echo "Removed any unneeded packages."
