ccc210ebdaacf0987ed8fdef34ec71dbafce3dd6
purplebeartoo <bcrypt@proton.me>
2025-07-27 18:34:17 -0500

    +new-install.sh

---
+++ .local/bin/new-install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# New install
+
+# Create a directory for the downloaded files
+mkdir -p ~/tmpscripts
+
+# Function to download and check the success of each file
+download_file() {
+  local url=$1
+  local destination=$2
+
+  # Check if the download was successful directly
+  if ! wget --quiet -O "$destination" "$url"; then
+    echo "Failed to download $url"
+    exit 1
+  fi
+
+  # Make the script executable (only for scripts)
+  if [[ "${url##*.}" == "sh" ]]; then
+    chmod +x "$destination"
+  fi
+}
+
+# Download and install the scripts
+download_file https://raw.githubusercontent.com/purplebeartoo/dotz/master/.local/bin/admin-write-config.sh ~/tmpscripts/awc
+download_file https://raw.githubusercontent.com/purplebeartoo/dotz/master/.local/bin/hyprland-from-source.sh ~/tmpscripts/hfs
+download_file https://raw.githubusercontent.com/purplebeartoo/dotz/master/.local/bin/package-install.sh ~/tmpscripts/pkginstall
+download_file https://raw.githubusercontent.com/purplebeartoo/dotz/master/.local/bin/user-write-config.sh ~/tmpscripts/uwc
+
+# Download the notes file (without making it executable)
+download_file https://raw.githubusercontent.com/purplebeartoo/dotz/master/Documents/linuxnotes.txt ~/linuxnotes
+
+echo "Download and installation completed successfully."
