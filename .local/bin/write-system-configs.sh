670c2b3779787ca264e463a06177cb025d80d0da
purplebeartoo <bcrypt@proton.me>
2025-10-04 18:28:56 -0500

    rm cmus

--- .local/bin/write-system-configs.sh
+++ .local/bin/write-system-configs.sh
@@ -11,7 +11,6 @@ err() { echo -e "\033[1;31mError: $*\033[0m" >&2; exit 1; }
 # Sync configuration from backup
 log "Syncing configuration from backup..."
 rsync -a "$HOME"/Downloads/.local/share/{bubblejail,zoxide} "$HOME"/.local/share || err "User share directories sync failed."
-rsync -a "$HOME"/Downloads/.config/cmus/playlists "$HOME"/.config/cmus || err "Cmus playlists sync failed."
 rsync -a "$HOME"/Downloads/.config/{BraveSoftware,chromium,Kvantum,nextdns} "$HOME"/.config || err "Config directories sync failed."
 rsync -a "$HOME"/Downloads/{.ssh,BrowserProfiles,Packages,Pictures,Playlists,Videos} "$HOME" || err "User directories sync failed."
 rsync -a --exclude "linuxnotes.txt" "$HOME"/Downloads/Documents "$HOME" || err "Documents sync failed."
