5495791cd0cef3e4a7b329f0bf2a20ec3a167354
purplebeartoo <bcrypt@proton.me>
2025-10-20 16:10:43 -0500

    mod cursor style and colors

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,17 @@
 local wezterm = require('wezterm')
+
 return {
   color_scheme = 'tokyonight_night',
+  default_cursor_style = 'SteadyUnderline',
   enable_tab_bar = false,
-  font = wezterm.font 'CaskaydiaCove Nerd Font',
+  font = wezterm.font('CaskaydiaCove Nerd Font'), -- Corrected font assignment
   font_size = 11.0,
   window_close_confirmation = 'NeverPrompt',
+  colors = {
+    cursor_bg = '#e0af68',
+    cursor_fg = '#1a1b26',
+    cursor_border = '#e0af68',
+  },
   window_padding = {
     top = 10,
     right = 15,
