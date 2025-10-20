20a44f17a9356c86e23b6006c183f27d79882348
purplebeartoo <bcrypt@proton.me>
2025-10-19 21:16:02 -0500

    +Wezterm-git for Wayland rendering

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require('wezterm')
 return {
   color_scheme = 'tokyonight_night',
   enable_tab_bar = false,
-  enable_wayland = false,
   font = wezterm.font 'CaskaydiaCove Nerd Font',
   font_size = 11.0,
   window_close_confirmation = 'NeverPrompt',
