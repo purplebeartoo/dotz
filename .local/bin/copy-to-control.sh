37548c57de3da3bb6991ade1bd81379701c3fcf3
purplebeartoo <bcrypt@proton.me>
2026-01-16 00:00:26 -0600

    add firefox profile copying

--- .local/bin/copy-to-control.sh
+++ .local/bin/copy-to-control.sh
@@ -15,12 +15,16 @@ ccache="$HOME/.cache/chromium"
 cconf="$HOME/.config/chromium"
 ccontrol="$HOME/BrowserProfiles/chromium"
 
-# Function to copy profile
+fcache="$HOME/.cache/mozilla"
+fconf="$HOME/.config/mozilla"
+fcontrol="$HOME/BrowserProfiles/mozilla"
+
+# Function to copy a profile using rsync
 copy_profile() {
   local source="$1"
   local target="$2"
-  echo "Copying $source profile..."
-  cp -r "$source" "$target"
+  echo "Syncing $source profile to $target …"
+  rsync -avzq --delete "$source" "$target"
 }
 
 # Check and copy Chromium profile
@@ -28,8 +32,19 @@ if [ -d "$cconf" ]; then
   echo "Removing existing Chromium cache and control directories..."
   rm -rf "$ccache" "$ccontrol"
 
-  copy_profile "$cconf" "$dest"
+  copy_profile "$cconf" "$dest/chromium"
   echo "Chromium config copied to control."
 else
   echo "Chromium source directory not present!"
+fi
+
+# Check and copy Firefox profile
+if [ -d "$fconf" ]; then
+  echo "Removing existing Firefox cache and control directories..."
+  rm -rf "$fcache" "$fcontrol"
+
+  copy_profile "$fconf" "$dest/mozilla"
+  echo "Firefox config copied to control."
+else
+  echo "Firefox source directory not present!"
 fi
